c20952e9c4ddefbae1ecd8dd1ffb43477eb36339
stem <zaregenilsya@yandex.by>
2023-04-13 21:42:17 +0300

    crosshair-pulse-reworked

--- Assets/Scripts/UI/CrosshairController.cs
+++ Assets/Scripts/UI/CrosshairController.cs
@@ -7,25 +7,30 @@ public class CrosshairController : MonoBehaviour, BeatReactor
 {
     [SerializeField] private Image _rightScopePrefab;
     [SerializeField] private Image _leftScopePrefab;
-
+    [SerializeField] private Image _staticLeftScope;
+    [SerializeField] private Image _staticRightScope;
     [SerializeField] private RectTransform _leftSpawnPosition;
     [SerializeField] private RectTransform _rightSpawnPosition;
-    [SerializeField] private RectTransform _leftEndPosition;
-    [SerializeField] private RectTransform _rightEndPosition;
+       
 
-    [SerializeField] private Color _endColor;
-    [SerializeField] private Color _spawnColor;
-    [SerializeField] private Color _hitInBeatColor;
+    [SerializeField] private Color _afterHitColorS;
+    [SerializeField] private Color _defaultColorS;
+
+    [SerializeField] private Color _startPosColorD;
+    [SerializeField] private Color _endPosColorD;
 
     [SerializeField] private float _rescaleSize = 0.7f;
-    private Vector3 _originalScale;
+    private Vector3 _defaultScale;
 
     private Image[] _leftScopes;
     private Image[] _rightScopes;
 
+    private RectTransform _endPosition;
+
     private const int _scopesCount = 3;    
     private int _currentScopes = 0;
     private bool _hitInBeatFixed = false;
+    private bool _staticScopesChanged = false;
 
     private Vector3 _extremePointDistance;
 
@@ -33,52 +38,64 @@ public class CrosshairController : MonoBehaviour, BeatReactor
     public void SetNewBeatState()
     {
         UpdateBeatState(0);
-        ScopesReset(_currentScopes);
+
+        if(!_hitInBeatFixed)
+            ResetDynamicScopes(_currentScopes);
+        else 
+            _hitInBeatFixed = false;
+
         _currentScopes = _currentScopes < _scopesCount - 1 ? _currentScopes + 1 : 0;
-        _hitInBeatFixed = false;
     }
 
 
     public void UpdateBeatState(float sampleShift)
     {
-        ShiftPosition(sampleShift);
-        ColorShift(sampleShift);
+        if (!_hitInBeatFixed)
+        {
+            ShiftPosition(sampleShift);
+            ColorShift(sampleShift);
+        }
+        else ResetDynamicScopes(_currentScopes);
+
+        if (_staticScopesChanged && !_hitInBeatFixed && sampleShift > 0.2f)
+        {
+            ResetStaticScopes();
+            _staticScopesChanged = false;
+        }
     }
 
 
-    private void ScopesReset(int scopesNumber)
+    private void ResetDynamicScopes(int scopesNumber)
     {
-        _leftScopes[scopesNumber].color = _rightScopes[scopesNumber].color = _spawnColor;
-
-        _leftScopes[scopesNumber].transform.localScale = _rightScopes[scopesNumber].transform.localScale = _originalScale;
-
         _leftScopes[scopesNumber].rectTransform.position = _leftSpawnPosition.position;
         _rightScopes[scopesNumber].rectTransform.position = _rightSpawnPosition.position;
+
+        _rightScopes[scopesNumber].color = _leftScopes[scopesNumber].color = _startPosColorD;
+    }
+
+
+    private void ResetStaticScopes()
+    {
+        _staticLeftScope.color = _staticRightScope.color = _defaultColorS;
+        _staticLeftScope.transform.localScale = _staticRightScope.transform.localScale = _defaultScale;
     }
     
 
     private void ShiftPosition(float sampleShift)
     {
-        if (!_hitInBeatFixed)
-        {
-            var shiftPosition = _extremePointDistance * sampleShift;
+        var shiftPosition = _extremePointDistance * sampleShift;
 
-            _leftScopes[_currentScopes].rectTransform.position = _leftSpawnPosition.position + shiftPosition;
-            _rightScopes[_currentScopes].rectTransform.position = _rightSpawnPosition.position - shiftPosition;
-        }
+        _leftScopes[_currentScopes].rectTransform.position = _leftSpawnPosition.position + shiftPosition;
+        _rightScopes[_currentScopes].rectTransform.position = _rightSpawnPosition.position - shiftPosition;
     }
 
 
     private void ColorShift(float sampleShift)
     {
+        var colorUpdate = Color.Lerp(_startPosColorD, _endPosColorD, sampleShift);
 
-        if (!_hitInBeatFixed)
-        {
-            var colorUpdate = Color.Lerp(_spawnColor, _endColor, sampleShift);
-
-            _leftScopes[_currentScopes].color = colorUpdate;
-            _rightScopes[_currentScopes].color = colorUpdate;
-        }
+        _leftScopes[_currentScopes].color = colorUpdate;
+        _rightScopes[_currentScopes].color = colorUpdate;
     }
 
 
@@ -86,31 +103,46 @@ public class CrosshairController : MonoBehaviour, BeatReactor
     {
         var scaleUpdate = new Vector3(_rescaleSize, _rescaleSize, 1);
 
-        _leftScopes[_currentScopes].transform.localScale = _rightScopes[_currentScopes].transform.localScale = scaleUpdate;
-        _leftScopes[_currentScopes].color = _hitInBeatColor;
-        _rightScopes[_currentScopes].color = _hitInBeatColor;
+        _staticLeftScope.transform.localScale = _staticRightScope.transform.localScale = scaleUpdate;
+        _staticLeftScope.color = _afterHitColorS;
+        _staticRightScope.color = _afterHitColorS;
 
         _hitInBeatFixed = true;
+        _staticScopesChanged = true;
     }
 
 
     private void InitializeComponents()
     {
-        _spawnColor = _rightScopePrefab.color;
-        _spawnColor.a = 0f;
+        _startPosColorD.a = 0f;
+        _endPosColorD.a = 1f;
 
         _leftScopes = new Image[_scopesCount];
         _rightScopes = new Image[_scopesCount];
 
-        _extremePointDistance = _leftEndPosition.position - _leftSpawnPosition.position;
-        _originalScale = _leftScopePrefab.transform.localScale;
+        InitializeEndPositions();
 
+        _extremePointDistance = _endPosition.position - _leftSpawnPosition.position;
+        _defaultScale = _leftScopePrefab.transform.localScale;
+
+        FillScopesArrays();
+    }
+
+
+    private void InitializeEndPositions()
+    {
+        _endPosition = _staticLeftScope.rectTransform;
+    }
+
+
+    private void FillScopesArrays()
+    {
         for (int i = 0; i < _scopesCount; i++)
         {
             _leftScopes[i] = (Instantiate(_leftScopePrefab, transform));
             _rightScopes[i] = (Instantiate(_rightScopePrefab, transform));
 
-            _rightScopes[i].color = _leftScopes[i].color = _spawnColor;
+            _rightScopes[i].color = _leftScopes[i].color = _startPosColorD;
 
             _rightScopes[i].rectTransform.position = _rightSpawnPosition.position;
             _leftScopes[i].rectTransform.position = _leftSpawnPosition.position;
